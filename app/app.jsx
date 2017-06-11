f5e770be8cfdc3917f4ed823904806271c832019
Rakesh <rakeshpetit@gmail.com>
2017-06-11 10:49:38 +0530

    Boilerplate for React

---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+$(document).foundation();
+
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+<p>React Boilerplate</p>,
+  document.getElementById('app')
+);
